26d0c05a9e0b28e43ab5bc649aff647af28416d6
Jeremiah <blitzyjp1@gmail.com>
2025-11-30 00:29:11 -0500

    Added scene transition from cutscene to classroom

---
+++ FinalGame/Assets/Jeremiah/JP_Scripts/LevelLoader.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    [SerializeField] Animator transition;
+    [SerializeField] VideoPlayer videoPlayer;  
+    [SerializeField] float transitionTime = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (videoPlayer != null)
+        {           
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        // Automatically load next scene when video ends
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+       StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
